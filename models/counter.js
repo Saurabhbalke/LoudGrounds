9ab155fe70438943b56856b1bf7eb04f521d10f0
saurabh Balke <20bcs201@iiitdmj.ac.in>
2024-03-19 19:48:37 +0530

    auto increment of user_id

--- models/counter.js
+++ models/counter.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-let counterSchema = new Schema({}, { strict: false });
+let counterSchema = new Schema({
+    key: {type: String},
+    seq: {type: Number}
+}, { strict: false });
 const counter = mongoose.model('counter', counterSchema);
 
 exports.getNextSequence = async (name) => {
     
-    await counter.findOneAndUpdate({ _id: name }, { $inc: { seq: 1 } }, { upsert: true, returnDocument: 'after' });
-      
-    return 2;
+    const data = await counter.findOneAndUpdate({ key: name }, { $inc: { seq: 1 } }, { upsert: true, returnDocument: 'after' });
+    
+    return data.seq;
  }
